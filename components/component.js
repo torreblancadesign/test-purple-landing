6bfbe67587275a785d2a8a87da26d1a12f72f61d
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-22 11:39:05 -0700

    updated landing page

--- components/component.js
+++ components/component.js
@@ -3,7 +3,7 @@ import styles from "../styles/style.module.css";
 
 const Component = () => {
   return (
-    <div style={{backgroundColor: "black", height: "1000px"}}>
+    <div style={{backgroundColor: "black", minHeight: "100vh"}}>
       <nav className={styles.navbar}>
         <div className={styles.navLeft}>
           <h1>Test Landing</h1>
@@ -17,6 +17,12 @@ const Component = () => {
       <div className={styles.hero}>
         <h2>Test Landing</h2>
       </div>
+      <div className={styles.paragraph}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu metus a massa ultrices ullamcorper vel id elit. Praesent bibendum purus eget dapibus iaculis. </p>
+      </div>
+      <footer className={styles.footer}>
+        <p>Copyright &copy; 2021 Test Landing</p>
+      </footer>
     </div>
   );
 };
