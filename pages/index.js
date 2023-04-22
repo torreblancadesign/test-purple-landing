2eaa3ac2cfd5c938adb19d2ec36365b1bd7f65e3
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-21 21:40:56 -0700

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,1 @@
+import React from 'react'; import Component from '../components/component'; const LoginPage = () => {   return (     <div>       <Component />     </div>   ); } export default LoginPage;
